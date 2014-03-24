cc31bfd99a35b725b046df8ebe2066bac3049f8c
hackyourlife <root@hackyourlife>
2014-03-24 22:54:46 +0100

    choice improvements

--- choice.py
+++ choice.py
@@ -4,16 +4,18 @@ import re
 from utils import oneof
 
 def choice(s):
-	r = r'^((hm+)?\.*\s+)?([a-zA-ZäöüÄÖÜß-]+)\soder\s([a-zA-ZäöüÄÖÜß-]+)\?'
+	r = r'^((hm+)?\.*\s+|.*:\s+)?([\sa-zA-ZäöüÄÖÜß-]+)\soder\s([\sa-zA-ZäöüÄÖÜß-]+)\?'
 	match = re.match(r, s)
 	if match:
 		print(match.groups())
 		return [ match.group(3), match.group(4) ]
-	r = r'((hm+)?\.*\s+|.*:\s+)?([a-zA-ZäöüÄÖÜß-]+(,\s[a-zA-ZäöüAÖÜß-]+)+\soder\s[a-zA-ZäöüÄÖÜß-]+)\?'
+	r = r'((hm+)?\.*\s+|.*:\s+)?([\sa-zA-ZäöüÄÖÜß-]+(,\s[\sa-zA-ZäöüAÖÜß-]+)+,?\soder\s[\sa-zA-ZäöüÄÖÜß-]+)\?'
 	match = re.match(r, s)
 	if match:
 		data = [ x.strip() for x in match.group(3).split(",") ]
-		fields = data[:-1] + data[-1].split(" oder ")
+		fields = data[:-1] + [ x.strip()
+				for x in data[-1].split("oder ")
+				if len(x.strip()) > 0 ]
 		return fields
 	return None
 
