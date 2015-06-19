aaae59dc2ad79c04989a81a1b3f176ec576b7c84
hackyourlife <root@hackyourlife>
2015-06-19 02:39:50 +0200

    handle "Orakel: ping"

--- pingpong.py
+++ pingpong.py
@@ -1,7 +1,7 @@
 # -*- coding: utf-8 -*-
 # vim:set ts=8 sts=8 sw=8 tw=80 noet cc=80:
 
-from module import Module, MUC
+from module import Module, MUC, MUC_MENTION
 from utils import oneof
 
 class PingPong(Module):
@@ -18,7 +18,7 @@ class PingPong(Module):
 
 
 	def __init__(self, **keywords):
-		super(PingPong, self).__init__([MUC], **keywords)
+		super(PingPong, self).__init__([MUC, MUC_MENTION], **keywords)
 
 	def muc_msg(self, msg, nick, **keywords):
 		key = msg.lower()
@@ -27,3 +27,13 @@ class PingPong(Module):
 				self.send_muc(oneof(self.users[nick]))
 			else:
 				self.send_muc(self.mapping[key])
+
+	def muc_mention(self, msg, nick, **keywords):
+		key = msg.lower()
+		if key in self.mapping:
+			if nick in self.users:
+				self.send_muc("%s: %s" % \
+						(nick, oneof(self.users[nick])))
+			else:
+				self.send_muc("%s: %s" % \
+						(nick, self.mapping[key]))
